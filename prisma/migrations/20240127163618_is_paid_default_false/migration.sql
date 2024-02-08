7b4701c88874885b9e4e144759327a52d8a7ec72
Brian Portatil <eltiobryanz@gmail.com>
2024-02-08 15:13:15 -0500

    Errores produccion

--- prisma/migrations/20240127163618_is_paid_default_false/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "UserAddress" DROP CONSTRAINT "UserAddress_userId_fkey";
-
--- AlterTable
-ALTER TABLE "Order" ALTER COLUMN "isPaid" SET DEFAULT false;
-
--- AddForeignKey
-ALTER TABLE "UserAddress" ADD CONSTRAINT "UserAddress_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
